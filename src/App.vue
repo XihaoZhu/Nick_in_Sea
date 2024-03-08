5573999edcb92642e6b6d98263071dc8fe73ea05
Xihao Zhu <xihao.zhu@outlook.com>
2024-03-08 17:02:07 +0000

    why页面动画初步雏形

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   import { useDataStore } from "./stores/data"
   import whoPart from './views/who/whoIndex.vue'
   import whatPart from './views/what/whatIndex.vue'
+  import whyPart from './views/why/whyIndex.vue'
 
   let dataStore = useDataStore()
   const directions=[["Who","50% 0"],["What","80% 100%"],["Why","20% 100%"],["Where","50% 100%"],[]];
@@ -29,10 +30,7 @@
     <whatPart></whatPart>
 
     <!-- why page -->
-    <div class="what" v-show="(dataStore.titlesShow == 2)&&dataStore.number==3">
-      <div class="content"></div>
-    <div class="backButton" @click="dataStore.getBack()">Clike to take you back for now</div>
-    </div>
+    <whyPart></whyPart>
 
     <!-- where page -->
     <div class="what" v-show="(dataStore.titlesShow == 2)&&dataStore.number==4">
---
+++ src/views/why/whyIndex.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useDataStore } from "../../stores/data"
+
+let dataStore = useDataStore()
+</script>
+
+<template>
+  <div v-show="(dataStore.titlesShow == 2)&&dataStore.number==3">
+      <div class="why">
+        <div class="container">
+          <div class="words">
+            Nick
+            Happy
+            Juicy
+          </div>
+        </div>
+        <div class="container"></div>
+        <div class="container"></div>
+      </div>
+    <div class="backButton" @click="dataStore.getBack()"></div>
+    </div>
+</template>
+
+<style src="./whyIndex.css" scoped>
+
+</style>
